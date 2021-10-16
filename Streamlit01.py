0a0143714bcad4b9db1c1e9b002d9163f86842a2
MrNightCrypt <91932788+MrNightCrypt@users.noreply.github.com>
2021-10-16 23:03:57 +0630

    Add files via upload

---
+++ Streamlit01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 22:47:51 2021
+
+@author: acer
+"""
+
+import streamlit as st
+
+st.write("""
+# Testing Streamlit Web app
+##### Finding Squared Number of User Interface with slider
+         
+    Hello...! This is a test of very first my streamlit web app
+    မင္ဂလာပါ ပထမဆုံး ဝက္ဘ္ဆိုဒ္ စမ္းသပ္ျခင္းပါ...။
+         """)
+# ျ  = s
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
